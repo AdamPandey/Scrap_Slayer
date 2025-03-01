5cbb2929f3e4011d6e5ed681adc18f710e2d3016
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-01 06:00:00 +0000

    v0.2.0 - Added meat allocation route

--- API/routes/api.php
+++ API/routes/api.php
@@ -11,3 +11,4 @@ Route::prefix('v1')->group(function () {
 });// Commit 0: v0.0.0 - Minor feature added of Food waste - 03/23/2025 12:27:25
 // Commit 1: v0.1.0 - Corrected goblin spawn logic - 03/23/2025 12:27:25
 // Commit 2: v0.1.1 - Minor feature added of Food waste - 03/23/2025 12:27:26
+// Commit 3: v0.2.0 - Added meat allocation route - 03/23/2025 12:27:26
