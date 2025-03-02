28ecad0f83bd9d99035b96d6f60976bd7ec574e1
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-02 16:00:00 +0000

    v0.12.0 - Added meat allocation route

--- API/routes/api.php
+++ API/routes/api.php
@@ -28,3 +28,4 @@ Route::prefix('v1')->group(function () {
 // Commit 17: v0.9.0 - Added meat allocation route - 03/23/2025 12:27:27
 // Commit 18: v0.10.0 - Added meat allocation route - 03/23/2025 12:27:27
 // Commit 19: v0.11.0 - Implemented goblin spawn API - 03/23/2025 12:27:28
+// Commit 20: v0.12.0 - Added meat allocation route - 03/23/2025 12:27:28
