0ca598dafb210b12423f2ad2ad9f3fb122f756d5
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-06 06:00:00 +0000

    v0.35.1 - Corrected goblin spawn logic

--- API/routes/api.php
+++ API/routes/api.php
@@ -71,3 +71,4 @@ Route::prefix('v1')->group(function () {
 // Commit 60: v0.33.1 - Added meat allocation route - 03/23/2025 12:27:32
 // Commit 61: v0.34.0 - Added Quants tracking endpoint - 03/23/2025 12:27:33
 // Commit 62: v0.35.0 - Fixed gold counter mismatch - 03/23/2025 12:27:33
+// Commit 63: v0.35.1 - Corrected goblin spawn logic - 03/23/2025 12:27:33
