02c48dbdc6c6a2559d237619a7998620ac75cc3a
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-07 12:00:00 +0000

    v1.0.3 - Integrated gold counter endpoint

--- API/routes/api.php
+++ API/routes/api.php
@@ -86,3 +86,4 @@ Route::prefix('v1')->group(function () {
 // Commit 75: v1.0.0 - Corrected goblin spawn logic - 03/23/2025 12:27:34
 // Commit 76: v1.0.1 - Resolved meat allocation error - 03/23/2025 12:27:34
 // Commit 77: v1.0.2 - Resolved meat allocation error - 03/23/2025 12:27:35
+// Commit 78: v1.0.3 - Integrated gold counter endpoint - 03/23/2025 12:27:35
