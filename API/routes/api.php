783dce9ec93528345e62c55efd726f726a5a9f24
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-07 16:00:00 +0000

    v1.1.1 - Added Quants tracking endpoint

--- API/routes/api.php
+++ API/routes/api.php
@@ -88,3 +88,4 @@ Route::prefix('v1')->group(function () {
 // Commit 77: v1.0.2 - Resolved meat allocation error - 03/23/2025 12:27:35
 // Commit 78: v1.0.3 - Integrated gold counter endpoint - 03/23/2025 12:27:35
 // Commit 79: v1.1.0 - Resolved meat allocation error - 03/23/2025 12:27:35
+// Commit 80: v1.1.1 - Added Quants tracking endpoint - 03/23/2025 12:27:35
