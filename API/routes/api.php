f24f3d52c0d01868549ca56fd5be78ce894637b2
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-10 08:00:00 +0000

    v1.17.2 - Rest API bug fixed

--- API/routes/api.php
+++ API/routes/api.php
@@ -120,3 +120,4 @@ Route::prefix('v1')->group(function () {
 // Commit 109: v1.16.1 - Added Quants tracking endpoint - 03/23/2025 12:27:38
 // Commit 110: v1.17.0 - Corrected goblin spawn logic - 03/23/2025 12:27:38
 // Commit 111: v1.17.1 - Resolved meat allocation error - 03/23/2025 12:27:39
+// Commit 112: v1.17.2 - Rest API bug fixed - 03/23/2025 12:27:39
