bc2922beedfa6d2579f664a8e3ceecb2dd647dea
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-10 12:00:00 +0000

    v1.18.0 - Added meat allocation route

--- API/routes/api.php
+++ API/routes/api.php
@@ -122,3 +122,4 @@ Route::prefix('v1')->group(function () {
 // Commit 111: v1.17.1 - Resolved meat allocation error - 03/23/2025 12:27:39
 // Commit 112: v1.17.2 - Rest API bug fixed - 03/23/2025 12:27:39
 // Commit 113: v1.17.3 - Minor feature added of Food waste - 03/23/2025 12:27:39
+// Commit 114: v1.18.0 - Added meat allocation route - 03/23/2025 12:27:39
