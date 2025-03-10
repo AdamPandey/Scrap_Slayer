ae6d488566d5530c99353bb9f95df2f0f76a1add
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-10 06:00:00 +0000

    v1.17.1 - Resolved meat allocation error

--- API/routes/api.php
+++ API/routes/api.php
@@ -119,3 +119,4 @@ Route::prefix('v1')->group(function () {
 // Commit 108: v1.16.0 - Resolved meat allocation error - 03/23/2025 12:27:38
 // Commit 109: v1.16.1 - Added Quants tracking endpoint - 03/23/2025 12:27:38
 // Commit 110: v1.17.0 - Corrected goblin spawn logic - 03/23/2025 12:27:38
+// Commit 111: v1.17.1 - Resolved meat allocation error - 03/23/2025 12:27:39
