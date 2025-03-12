b987bf98ba6bee2bd6369655274e60ec78c8b8e3
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-12 12:00:00 +0000

    v1.28.2 - Implemented goblin spawn API

--- API/routes/api.php
+++ API/routes/api.php
@@ -146,3 +146,4 @@ Route::prefix('v1')->group(function () {
 // Commit 135: v1.27.0 - Implemented goblin spawn API - 03/23/2025 12:27:41
 // Commit 136: v1.28.0 - Rest API bug fixed - 03/23/2025 12:27:42
 // Commit 137: v1.28.1 - Fixed Quants overflow in response - 03/23/2025 12:27:42
+// Commit 138: v1.28.2 - Implemented goblin spawn API - 03/23/2025 12:27:42
