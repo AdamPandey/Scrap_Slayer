a2dd4b7d18bb4211b436e2a2fd993a255abe2e27
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-12 20:00:00 +0000

    v1.30.1 - Fixed gold counter mismatch

--- API/routes/api.php
+++ API/routes/api.php
@@ -150,3 +150,4 @@ Route::prefix('v1')->group(function () {
 // Commit 139: v1.29.0 - Fixed Quants overflow in response - 03/23/2025 12:27:42
 // Commit 140: v1.29.1 - Added meat allocation route - 03/23/2025 12:27:42
 // Commit 141: v1.30.0 - Fixed gold counter mismatch - 03/23/2025 12:27:42
+// Commit 142: v1.30.1 - Fixed gold counter mismatch - 03/23/2025 12:27:42
