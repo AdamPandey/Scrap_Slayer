8785e2a6b2a15a57df3524fd03a3c43b71f660df
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-23 10:00:00 +0000

    v2.4.4 - Implemented Quants collection system

--- Assets/player.cs
+++ Assets/player.cs
@@ -8,3 +8,4 @@
 // Commit 7: v2.4.1 - Resolved goblin collision issue - 03/23/2025 12:57:12
 // Commit 8: v2.4.2 - Corrected Quants calculation error - 03/23/2025 12:57:12
 // Commit 9: v2.4.3 - Corrected Quants calculation error - 03/23/2025 12:57:12
+// Commit 10: v2.4.4 - Implemented Quants collection system - 03/23/2025 12:57:12
