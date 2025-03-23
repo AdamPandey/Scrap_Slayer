2d3745924d276e1403f8d41d97a3ef3e1c6376f6
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-23 13:00:00 +0000

    v2.5.2 - Added player movement mechanics

--- Assets/player.cs
+++ Assets/player.cs
@@ -11,3 +11,4 @@
 // Commit 10: v2.4.4 - Implemented Quants collection system - 03/23/2025 12:57:12
 // Commit 11: v2.5.0 - Corrected Quants calculation error - 03/23/2025 12:57:12
 // Commit 12: v2.5.1 - Adjusted gold counter display - 03/23/2025 12:57:12
+// Commit 13: v2.5.2 - Added player movement mechanics - 03/23/2025 12:57:12
