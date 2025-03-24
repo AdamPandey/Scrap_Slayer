24d1abc3ad8d78e52f14edcdb6f3e2ae33be25ec
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-24 10:00:00 +0000

    v2.10.1 - Adjusted gold counter display

--- Assets/player.cs
+++ Assets/player.cs
@@ -25,3 +25,4 @@
 // Commit 24: v2.8.3 - Added goblin interaction logic - 03/23/2025 12:57:14
 // Commit 25: v2.9.0 - Added goblin interaction logic - 03/23/2025 12:57:14
 // Commit 26: v2.10.0 - Fixed meat allocation glitch - 03/23/2025 12:57:14
+// Commit 27: v2.10.1 - Adjusted gold counter display - 03/23/2025 12:57:14
