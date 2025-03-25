d8a6df4e8bfa4bed0b998d8ffbd86ad63e848f1d
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-25 12:00:00 +0000

    v2.20.0 - Fixed meat allocation glitch

--- Assets/player.cs
+++ Assets/player.cs
@@ -44,3 +44,4 @@
 // Commit 43: v2.19.2 - Adjusted gold counter display - 03/23/2025 12:57:16
 // Commit 44: v2.19.3 - Adjusted gold counter display - 03/23/2025 12:57:16
 // Commit 45: v2.19.4 - Added goblin interaction logic - 03/23/2025 12:57:16
+// Commit 46: v2.20.0 - Fixed meat allocation glitch - 03/23/2025 12:57:16
