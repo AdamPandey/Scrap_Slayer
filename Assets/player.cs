27728935f1861d7160e16bc6f4fb603320a2c1d3
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-27 05:00:00 +0000

    v2.34.2 - Implemented Quants collection system

--- Assets/player.cs
+++ Assets/player.cs
@@ -71,3 +71,4 @@
 // Commit 70: v2.33.1 - Integrated meat allocation system - 03/23/2025 12:57:19
 // Commit 71: v2.34.0 - Resolved goblin collision issue - 03/23/2025 12:57:19
 // Commit 72: v2.34.1 - Fixed meat allocation glitch - 03/23/2025 12:57:19
+// Commit 73: v2.34.2 - Implemented Quants collection system - 03/23/2025 12:57:19
