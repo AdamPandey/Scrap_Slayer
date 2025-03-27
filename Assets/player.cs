517e900b431234face1349934e40e5c717efc346
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-27 16:00:00 +0000

    v3.5.1 - Fixed player movement bug

--- Assets/player.cs
+++ Assets/player.cs
@@ -82,3 +82,4 @@
 // Commit 81: v3.4.0 - Corrected Quants calculation error - 03/23/2025 12:57:20
 // Commit 82: v3.4.1 - Added player movement mechanics - 03/23/2025 12:57:20
 // Commit 83: v3.5.0 - Fixed player movement bug - 03/23/2025 12:57:20
+// Commit 84: v3.5.1 - Fixed player movement bug - 03/23/2025 12:57:21
