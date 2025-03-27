8cf553c07ceb17648c5e31bce6d33d21879120bc
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-27 02:00:00 +0000

    v2.33.1 - Integrated meat allocation system

--- Assets/player.cs
+++ Assets/player.cs
@@ -68,3 +68,4 @@
 // Commit 67: v2.32.1 - Resolved goblin collision issue - 03/23/2025 12:57:19
 // Commit 68: v2.32.2 - Integrated meat allocation system - 03/23/2025 12:57:19
 // Commit 69: v2.33.0 - Adjusted gold counter display - 03/23/2025 12:57:19
+// Commit 70: v2.33.1 - Integrated meat allocation system - 03/23/2025 12:57:19
