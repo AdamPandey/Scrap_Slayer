990a5417e783dbb98056abfbcfb230534f1e4115
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-28 13:00:00 +0000

    v3.9.1 - Fixed meat allocation glitch

--- Assets/player.cs
+++ Assets/player.cs
@@ -96,3 +96,4 @@
 // Commit 95: v3.8.0 - Resolved goblin collision issue - 03/23/2025 12:57:22
 // Commit 96: v3.8.1 - Added player movement mechanics - 03/23/2025 12:57:22
 // Commit 97: v3.9.0 - Corrected Quants calculation error - 03/23/2025 12:57:22
+// Commit 98: v3.9.1 - Fixed meat allocation glitch - 03/23/2025 12:57:22
