4e001837c916a4abd36ccf4d43c275d56a6d3ed6
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-29 01:00:00 +0000

    v3.11.2 - Integrated meat allocation system

--- Assets/player.cs
+++ Assets/player.cs
@@ -101,3 +101,4 @@
 // Commit 100: v3.10.0 - Integrated meat allocation system - 03/23/2025 12:57:22
 // Commit 101: v3.11.0 - Resolved goblin collision issue - 03/23/2025 12:57:23
 // Commit 102: v3.11.1 - Resolved goblin collision issue - 03/23/2025 12:57:23
+// Commit 103: v3.11.2 - Integrated meat allocation system - 03/23/2025 12:57:23
