29f17636350504a56efcdea98e2bf07495bf4e64
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-29 05:00:00 +0000

    v3.15.0 - Implemented Quants collection system

--- Assets/player.cs
+++ Assets/player.cs
@@ -105,3 +105,4 @@
 // Commit 104: v3.12.0 - Implemented Quants collection system - 03/23/2025 12:57:23
 // Commit 105: v3.13.0 - Added goblin interaction logic - 03/23/2025 12:57:23
 // Commit 106: v3.14.0 - Integrated meat allocation system - 03/23/2025 12:57:23
+// Commit 107: v3.15.0 - Implemented Quants collection system - 03/23/2025 12:57:23
