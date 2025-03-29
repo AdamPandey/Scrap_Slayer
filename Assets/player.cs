a701132a00173d7c8ad5e5d528645375d02e7c94
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-29 07:00:00 +0000

    v3.16.1 - Added goblin interaction logic

--- Assets/player.cs
+++ Assets/player.cs
@@ -107,3 +107,4 @@
 // Commit 106: v3.14.0 - Integrated meat allocation system - 03/23/2025 12:57:23
 // Commit 107: v3.15.0 - Implemented Quants collection system - 03/23/2025 12:57:23
 // Commit 108: v3.16.0 - Adjusted gold counter display - 03/23/2025 12:57:23
+// Commit 109: v3.16.1 - Added goblin interaction logic - 03/23/2025 12:57:23
