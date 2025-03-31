b487c01313ace2d69b3c1dea5c345f1e97e074a9
N00234646@iadt.ie <N00234646@iadt.ie>
2025-03-31 07:00:00 +0100

    v3.32.3 - Integrated meat allocation system

--- Assets/player.cs
+++ Assets/player.cs
@@ -141,3 +141,4 @@
 // Commit 140: v3.32.0 - Corrected Quants calculation error - 03/23/2025 12:57:27
 // Commit 141: v3.32.1 - Adjusted gold counter display - 03/23/2025 12:57:27
 // Commit 142: v3.32.2 - Corrected Quants calculation error - 03/23/2025 12:57:27
+// Commit 143: v3.32.3 - Integrated meat allocation system - 03/23/2025 12:57:27
